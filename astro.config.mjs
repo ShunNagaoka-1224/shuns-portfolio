452df6cacfd55667c264de88ed3e3933d0433bcc
ShunNagaoka-1224 <nagaoka.shun.nu7@naist.ac.jp>
2026-07-25 14:42:32 +0900

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://ShunNagaoka-1224.github.io',
+  base: '/Portfolio-astro',
+});
