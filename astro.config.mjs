6b4a34fbdab4203d443faaac8426c636a01c57ac
ShunNagaoka-1224 <nagaoka.shun.nu7@naist.ac.jp>
2026-07-25 14:48:07 +0900

    Fix base path to /shuns-portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ShunNagaoka-1224.github.io',
-  base: '/Portfolio-astro',
+  base: '/shuns-portfolio',
 });
